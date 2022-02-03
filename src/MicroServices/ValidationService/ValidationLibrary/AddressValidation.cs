04a1f489704b3e9ae80c845bdf2d463789cfe2dc
ismi2u <ismailkhanf@gmail.com>
2022-02-04 03:00:01 +0530

    Validators Added

---
+++ src/MicroServices/ValidationService/ValidationLibrary/AddressValidation.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ValidationLibrary
+{
+    public class AddressValidation
+    {
+        private string _HouseNo { get; set; }
+        private string _Street { get; set; }
+        private string _City { get; set; }
+        private string _Region { get; set; }
+        private string _PostalCode { get; set; }
+        private string _Country { get; set; }
+        private string _CountryCode { get; set; }
+        private string _Language { get; set; }
+        private string _Locale { get; set; }
+        private string _AddressValidatorAPIKey { get; set; }
+
+        public AddressValidation(string HouseNo, string Street, string City,
+                            string Region, string PostalCode, string Country,
+                            string Language, string AddressValidatorAPIKey)
+        {
+            _HouseNo = HouseNo;
+            _Street = Street;
+            _City = City;
+            _Region = Region;
+            _PostalCode = PostalCode;
+            _Country = Country;
+            _Language = Language;
+            _AddressValidatorAPIKey = AddressValidatorAPIKey;
+        }
+
+        public string GetStreetAddress()
+        {
+            string StreetAddress = _HouseNo + " " + _Street;
+
+            return StreetAddress;
+        }
+
+        public string GetCity()
+        {
+            return _City;
+        }
+
+        public string GetRegion()
+        {
+            return _Region;//region is state
+        }
+        public string GetPostalCode()
+        {
+            return _PostalCode;
+        }
+        public string GetCountry()
+        {
+            return _Country;
+        }
+
+        public string GetCountryCode()
+        {
+            return _Country; //Convert the country  to country code
+        }
+        public string GetLanguage()
+        {
+            return _Language;
+        }
+
+        public string GetLocale()
+        {
+            return _Language; //convet language to locale
+        }
+
+        public string GetAddressValidatorAPIKey()
+        {
+            return _AddressValidatorAPIKey;
+        }
+        public string ValidateStreetAddress()
+        {
+            const string APIURL = "https://api.address-validator.net/api/verify";
+            HttpClient client = new HttpClient();
+
+            var postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("StreetAddress", GetStreetAddress()));
+            postData.Add(new KeyValuePair<string, string>("City", GetCity()));
+            postData.Add(new KeyValuePair<string, string>("PostalCode", GetPostalCode()));
+            postData.Add(new KeyValuePair<string, string>("State", GetRegion()));
+            postData.Add(new KeyValuePair<string, string>("CountryCode", GetCountryCode()));
+            postData.Add(new KeyValuePair<string, string>("Locale", GetLocale()));
+            postData.Add(new KeyValuePair<string, string>("APIKey", GetAddressValidatorAPIKey()));
+
+            HttpContent content = new FormUrlEncodedContent(postData);
+
+            HttpResponseMessage result = client.PostAsync(APIURL, content).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            AddressAPIValidationResult res = JsonSerializer.Deserialize<AddressAPIValidationResult>(resultContent);
+            string formattedaddress = "";
+
+            if (res.Status.Equals("VALID"))
+            {
+                formattedaddress = res.FormattedAddress;
+            }
+            else
+            {
+                formattedaddress = "Address is not Valid";
+            }
+            return formattedaddress;
+        }
+    }
+
+
+    public class AddressAPIValidationResult
+    {
+        public string Status { get; set; }
+        public string FormattedAddress { get; set; }
+    }
+
+
+    public class AddressValidationInputs
+    {
+        public string HouseNo { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Language { get; set; }
+
+    }
+
+}
---
+++ src/MicroServices/VendorService/AtoVen.API/Controllers/ValidationController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using ValidationLibrary;
+
+namespace AtoVen.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValidationController : ControllerBase
+    {
+        [HttpPost]
+        [ActionName("VATValidator")]
+        public async Task<ActionResult<string>> VATValidator(string VATNumber)
+        {
+
+            const string VATAPIValidatorKey = "579db579789b253ddbc7708f84990f18";
+            //'sample = GB429214460'
+            string APIUrl = "http://www.apilayer.net/api/validate?access_key=" + VATAPIValidatorKey + "&vat_number=" + VATNumber;
+            HttpClient client = new HttpClient();
+            HttpResponseMessage result = client.PostAsync(APIUrl, null).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            //VATAPIValidationResult VATresult = JsonSerializer.Deserialize<VATAPIValidationResult>(resultContent);
+            return resultContent;
+
+        }
+
+        [HttpPost]
+        [ActionName("IBANValidator")]
+        public async Task<ActionResult<string>> IBANValidator(string IbanNumber)
+        {
+            //https://api.ibanapi.com/v1/validate/EE471000001020145685?api_key=API_KEY
+            const string IBANAPIValidatorKey = "a1cd8ac82a2940d9bb3f9fa20d84261ddd6cd538";
+            //ICICIBank IBAN number = DE56501201000000484637
+            string APIUrl =  "https://api.ibanapi.com/v1/validate/" + IbanNumber + "?api_key=" + IBANAPIValidatorKey;
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage result = client.PostAsync(APIUrl, null).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            return resultContent;
+
+        }
+
+        [HttpPost]
+        [ActionName("AddressValidator")]
+        public async Task<ActionResult<string>> AddressValidator(AddressValidationInputs addressValidationInputs)
+        {
+            const string APIURL = "https://api.address-validator.net/api/verify";
+            string APIKey = "av-46947951bfd581f1e1d6c206b2a8f9e0";
+
+            HttpClient client = new HttpClient();
+
+            var postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("StreetAddress", addressValidationInputs.HouseNo + " " + addressValidationInputs.Street));
+            postData.Add(new KeyValuePair<string, string>("City", addressValidationInputs.City));
+            postData.Add(new KeyValuePair<string, string>("PostalCode", addressValidationInputs.PostalCode));
+            postData.Add(new KeyValuePair<string, string>("State", addressValidationInputs.Region));
+            postData.Add(new KeyValuePair<string, string>("CountryCode", addressValidationInputs.Country));
+            postData.Add(new KeyValuePair<string, string>("Locale", addressValidationInputs.Language));
+            postData.Add(new KeyValuePair<string, string>("APIKey", APIKey));
+
+            HttpContent content = new FormUrlEncodedContent(postData);
+
+            HttpResponseMessage result = client.PostAsync(APIURL, content).Result;
+            string resultContent = result.Content.ReadAsStringAsync().Result;
+            //AddressAPIValidationResult res = JsonSerializer.Deserialize<AddressAPIValidationResult>(resultContent);
+            //string formattedaddress = "";
+
+            //if (res.Status.Equals("VALID"))
+            //{
+            //    formattedaddress = res.FormattedAddress;
+            //}
+            //else
+            //{
+            //    formattedaddress = "Address is not Valid";
+            //}
+            return resultContent;
+        }
+    }
+}
